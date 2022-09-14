c4ba19d400f3234d59e56c05143246a430118370
Evelyn <evelyncoronel05@gmail.com>
2022-09-14 20:50:10 -0300

    Entregable 4

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require(('express'))
+const path = require('path')
 const PORT = process.env.PORT || 8080;
 const apiRoutes = require('./routers/app.routers')
 const app = express()
 
 app.use(express.json());
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 
 app.use('/api/', apiRoutes)
 
@@ -16,5 +19,6 @@ const serverConnected = app.listen(PORT, () => {
     console.log(`Server is up and running`)
 })
 serverConnected.on('error', (error) => {
+
     console.log(error.message)
 })
--- routers/productos/products.routes.js
+++ routers/productos/products.routes.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-const productos = require('../../data/data.json')
-console.log(productos)
+let products = require('../../data/data.json')
 
 router.get('/', (req, res) => {
-    const products = JSON.parse(fs.readFileSync('../../data/data.json', 'utf-8'));
     res.json(products);
 })
 
 router.get('/:id', (req, res) => {
-    const products = JSON.parse(fs.readFileSync('../../data/data.json', 'utf-8'));
     const { id } = req.params;
     const product = products.find((product) => product.id === +(id));
     product ? res.json(product) : res.json({ error: 'producto no encontrado' })
 
 })
 router.post('/', (req, res) => {
-    const products = JSON.parse(fs.readFileSync('../../data/data.json', 'utf-8'));
+
     const { title, price, thumbnail } = req.body;
     const nuevoProducto = {
         id: products.length + 1,
@@ -26,12 +23,10 @@ router.post('/', (req, res) => {
         thumbnail,
     };
     products.push(nuevoProducto);
-    fs.writeFileSync('../../data/data.json', JSON.stringify(products, null, 2));
     res.json(nuevoProducto);
 })
 
 router.put('/:id', (req, res) => {
-    const products = JSON.parse(fs.readFileSync('../../data/data.json', 'utf-8'));
     const { id } = req.params;
     const { title, price, thumbnail } = req.body;
 
@@ -42,18 +37,17 @@ router.put('/:id', (req, res) => {
         thumbnail
     } : p);
 
-    console.log(productsUpdate)
+    products = productsUpdate
     res.json('Todo ok');
-    fs.writeFileSync('../../data/data.json', JSON.stringify(productsUpdate, null, 2));
+
 })
 
 router.delete('/:id', (req, res) => {
-    const products = JSON.parse(fs.readFileSync('../../data/data.json', 'utf-8'));
     const { id } = req.params;
     let newProducts = products.filter(item => item.id != +(id))
     console.log(newProducts)
     res.json('Todo ok');
-    fs.writeFileSync('../../data/data.json', JSON.stringify(newProducts, null, 2));
+    products = newProducts
 })
 
 
