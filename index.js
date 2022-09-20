dcf872cce8efad8d755e98a80935c84943a9d17b
Evelyn <evelyncoronel05@gmail.com>
2022-09-19 21:34:18 -0300

    Se implementa una clase para Products

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const express = require(('express'))
-const path = require('path')
-const PORT = process.env.PORT || 8080;
+const express = require('express')
 const apiRoutes = require('./routers/app.routers')
+const {engine} = require('express-handlebars')
+const path = require('path')
+
+const PORT = process.env.PORT || 8080;
 const app = express()
+
+app.engine('hbs', engine({
+    extname: 'hbs',
+    defaultLayout: 'main.hbs',
+    layoutsDir: path.resolve(__dirname, './views/layouts'),
+    partialsDir: path.resolve(__dirname, './views/partials')
+}))
+
+app.set('views', './views')
+app.set('view engine', 'hbs')
 
 app.use(express.json());
 app.use(express.static('public'))
@@ -11,7 +23,6 @@ app.use(express.urlencoded({extended:true}))
 app.use('/api/', apiRoutes)
 
 app.get('*', (req, res) => {
-
     res.status(404).send('Página no encontrada')
 })
 
@@ -19,6 +30,5 @@ const serverConnected = app.listen(PORT, () => {
     console.log(`Server is up and running`)
 })
 serverConnected.on('error', (error) => {
-
     console.log(error.message)
 })
--- routers/productos/products.routes.js
+++ routers/productos/products.routes.js
@@ -1,53 +1,57 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-let products = require('../../data/data.json')
+/* let products = require('../../data/data.json') */
+const Products = require('../../model/products')
+
+const products = new Products()
 
 router.get('/', (req, res) => {
-    res.json(products);
+    res.json(products.getAll());
 })
 
 router.get('/:id', (req, res) => {
     const { id } = req.params;
-    const product = products.find((product) => product.id === +(id));
-    product ? res.json(product) : res.json({ error: 'producto no encontrado' })
-
+    const product = products.getById(id)
+    if (product.error) {
+        res.status(404).json(product)
+    } else {
+        res.status(201).json(product)
+    }
 })
-router.post('/', (req, res) => {
 
-    const { title, price, thumbnail } = req.body;
-    const nuevoProducto = {
-        id: products.length + 1,
-        title,
-        price,
-        thumbnail,
-    };
-    products.push(nuevoProducto);
-    res.json(nuevoProducto);
+router.post('/', (req, res) => {
+    let newProduct = products.save(req.body)
+    if (newProduct.product) {
+        res.status(201).json(newProduct);
+    } else {
+        res.status(400).json(newProduct);
+    }
 })
 
 router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const { title, price, thumbnail } = req.body;
-
-    let productsUpdate = products.map((p) => p.id === +(id) ? p = {
-        id,
-        title,
-        price,
-        thumbnail
-    } : p);
-
-    products = productsUpdate
-    res.json('Todo ok');
-
+    let { id } = req.params;
+    id = Number(id)
+    let productsUpdate = products.updateById(id, req.body)
+    if (productsUpdate.error) {
+        res.status(400).json(productsUpdate)
+    } else {
+        res.status(200).json(productsUpdate)
+    }
 })
 
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
-    let newProducts = products.filter(item => item.id != +(id))
-    console.log(newProducts)
-    res.json('Todo ok');
+    /* let newProducts = products.filter(item => item.id != +(id))
     products = newProducts
+    res.status(200).json({ text: 'Deleted successfully' }) */
+    const product = products.getById(id)
+    if (product.error) {
+        res.status(404).json(product)
+    } else {
+        res.status(200).json(products.deleteById(id))
+    }
+
 })
 
 
